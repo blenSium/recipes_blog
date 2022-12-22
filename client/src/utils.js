d0485fec52a5d11b275b0da75c311a3a6a77b81a
shelly <shely2111@gmail.com>
2022-12-23 01:50:13 +0200

    get user function

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getUser = async (userId, set) => {
-    const { data } = await axios.get(`${process.env.REACT_APP_API}/users/${userId}`);
+    const { data } = await axios.get(`https://nice-erin-cricket-boot.cyclic.app/users/${userId}`);
     set(data);
   };
 
